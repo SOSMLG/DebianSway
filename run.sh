49ae11df270d061dbe66cb1a25aad414b6e4f494
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2026-01-07 21:48:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ SCRIPTS=(
     "SweetMars.sh|Install SweetMars|Y"
     "bibataCursor.sh|Install Bibata Cursors|Y"
     "addUserToGroups.sh|Add your user to the needed groups|Y"
-    "generalndgaming.sh|Peak Gaming And Coding Fix|Y"
+    "GeneralStuffAndGamingFix.sh|Peak Gaming And Coding Fix|Y"
 )
 
 
